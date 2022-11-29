218284ea38a4ff65931311251caba60be423353d
Korvo2Tuirow <korvo2irow@live.com>
2022-11-29 00:29:52 -0300

    js
    melhoria na aula

--- JAVASCRIPT/Modulo3/Aula 4 Manipulanso Elementos/script.js
+++ JAVASCRIPT/Modulo3/Aula 4 Manipulanso Elementos/script.js
@@ -3,22 +3,35 @@ function clicou(){
    console.log(teste.children[0].children);
    const ul = teste.querySelector('ul');
    console.log(ul);
+}
 
+
+function clicou2(){
+  const teste =document.querySelector('#teste');
    const teste2 = document.querySelector('#teste2');
    const ul2 = teste2.querySelector('ul');
    ul2.innerHTML = "<li> Item alterado</li>";
+}
 
 
+function clicou3(){
   const teste3 = document.querySelector('#teste3');
   const ul3 = teste3.querySelector('ul');
-  ul3.innerHTML += ul3.innerHTML + "<li><strong>Item alterado2</strong></li>";
-  ul3.children[1].innerText =  "<li><strong>Item alterado2</strong></li>";
+  ul3.innerHTML += ul3.innerHTML 
+  ul3.children[0].innerText =  "<li><strong>Item alterado2</strong></li>";
   
+}
+
+function clicou4(){
   const teste4 = document.querySelector('#teste4');
   const ul4 = teste4.querySelector('ul');
   ul4.children[0].append(" ola") // concatena
   ul4.append("ola")
+}
 
+
+function clicou5() {
+  
   const teste5 = document.querySelector('#teste5');
   const ul5 = teste5.querySelector('ul');
   let newLi = document.createElement("li");
@@ -28,23 +41,29 @@ function clicou(){
   let newText = document.createElement("h1");
   newText.innerText = "Texto novo";
   ul5.prepend(newText);
+}
 
-let btn = document.createElement("button");
-  btn.innerText = "olha eu ae";
-  ul3.after(btn);
+function clicou6(){
+const add = document.querySelector("body")  
+const btn = document.createElement("button")
+btn.innerText = "Novo Botão";
+add.append (btn) ;
+}
 
+
+function clicou7(){
   const newUl = document.createElement("ul");
-
-  for(let i = 0; i <5; i++){
+  const add = document.querySelector("body") 
+  for(let i = 1; i <5; i++){
    let newLi2 = document.createElement("li");
-   newLi2.innerHTML = "item adicionado" + i;
+   newLi2.innerHTML = "item adicionado" + ' ' +i;
    newUl.append(newLi2);
   }
   
-  ul5.after(newUl);
+  add.append(newUl);
+}
   
-  
- }   
+ 
 
 //after = depois
    //before = antes
